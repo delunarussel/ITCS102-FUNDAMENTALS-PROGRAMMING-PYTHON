2f2cff370d3d7791f1510bb6d0bdd08bcac98a4d
delunarussel <delunarussel996@gmail.com>
2025-08-28 22:22:17 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = 'Russel337'
+password = '123456789'
+
+name = input('Input username --->' )
+pass1 = input('Input password --->' )
+
+if username == name and password == pass1:
+	print("Access Granted")
+else:
+	print("Access Denied")
---
+++ code_challenge4.py
@@ -0,0 +1,6 @@
+number = int(input("Input number ---> "))
+
+if number % 2 == 0:
+    print("The number is EVEN")
+else:
+    print("The number is ODD")
