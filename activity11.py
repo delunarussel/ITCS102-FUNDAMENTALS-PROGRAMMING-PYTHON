3dd66cd2792b9947a448228f11e7c8d1d66bc7c0
delunarussel <delunarussel996@gmail.com>
2025-09-08 22:21:43 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,25 @@
+#create a program that accepts float value
+#determine temperature reading
+#1 to 20 cold
+#21 to 30 lukewarm
+#31 to 40 warm
+#41 to 50 hot
+#51 and above boiling hot
+temp = eval(input("Enter temperature --> "))
+
+if temp < 1: 
+	print("freezing cold temperature")
+elif temp> 1 and temp < 20: 
+	print("temperature outside is cold")
+elif temp > 21 and temp <= 30: 
+	print("temperature outside is lukewarm")
+elif temp> 31 and temp <= 40:
+	print("temperature outside is warm")
+elif temp> 41 and temp < 50: 
+	print("temperature outside is hot")
+elif temp > 51: 
+	print("temperature outside is boiling hot")
+
+else:
+	print("invalid temperature")
+
