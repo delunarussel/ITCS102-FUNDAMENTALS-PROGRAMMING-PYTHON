997620a121f08d1702b7c4fef6bc83b3bfb5b1ab
delunarussel <delunarussel996@gmail.com>
2025-09-12 22:04:47 +0800

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,7 @@
+#using for loop, ask user to input 10 numbers
+#after input, get the summation of all the numbers
+
+print("Hello World")
+
+for rdl in range(1, 21, 1):
+    print(rdl, "- Hello World")
